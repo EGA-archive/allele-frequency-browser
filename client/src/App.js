d58af93217d0d38fbc00f358a87ba8187094331d
costero-e <orildsaga@gmail.com>
2024-10-31 09:03:01 +0100

    uri changed

--- client/src/App.js
+++ client/src/App.js
@@ -45,7 +45,7 @@ function App () {
       let metaresponse;
       metaresponse = await axios({
         method: 'get',
-        url: `http://localhost:8080/beacon-network/v2.0.0/`,
+        url: `https://af-gdi-bn-api-demo.ega-archive.org/beacon-network/v2.0.0/`,
         //url: `http://localhost:8080/beacon-network/v2.0.0/beacon-network/v2.0.0/g_variants`,
         headers: {
           'Content-Type': 'application/json'
@@ -86,7 +86,7 @@ function App () {
         // console.log(auth)
       response = await axios({
         method: 'post',
-        url: `http://localhost:8080/beacon-network/v2.0.0/g_variants`,
+        url: `https://af-gdi-bn-api-demo.ega-archive.org/beacon-network/v2.0.0/g_variants`,
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${auth.userData.access_token}`
@@ -97,7 +97,7 @@ function App () {
     } else {
       response = await axios({
         method: 'get',
-        url: `http://localhost:8080/beacon-network/v2.0.0/g_variants?start=${arr[1]}&end=${finalend}&alternateBases=${arr[3]}&referenceBases=${arr[2]}&referenceName=${arr[0]}`,
+        url: `https://af-gdi-bn-api-demo.ega-archive.org/v2.0.0/g_variants?start=${arr[1]}&end=${finalend}&alternateBases=${arr[3]}&referenceBases=${arr[2]}&referenceName=${arr[0]}`,
         //url: `http://localhost:8080/beacon-network/v2.0.0/g_variants`,
         headers: {
           'Content-Type': 'application/json'
